aafec2805b4069c493275c3a95145b46d82b3003
Prof. Vanselow <profvanselow@gmail.com>
2018-11-27 19:11:47 -0500

    Added Exceptions

--- src/sandbox/Assert.java
+++ src/sandbox/Assert.java
@@ -3,7 +3,8 @@ package sandbox;
 public class Assert {
 
   public static void main(String[] args) {
-    // If you are using IntelliJ IDEA enable assert by passing VM Option '-ea' at following
+    // If you are using IntelliJ IDEA enable assert by passing
+    // VM Option '-ea' at following
     // Run > Edit Configurations... > Configuration > VM options:
     int x = 5;
     int y = 5;
@@ -12,8 +13,8 @@ public class Assert {
     } else if (x < y) {
       System.out.println("y is greater");
     } else {
-      //assert (x != y); // throws exception
-      assert (x == y);  // must be equal
+      assert (x != y); // throws exception
+      //assert (x == y);  // must be equal
     }
     System.out.println("after assert"); //
   }
---
+++ src/sandbox/Exceptions.java
@@ -0,0 +1,93 @@
+package sandbox;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class Exceptions {
+
+  public static void main(String[] args) {
+
+    basicExample();
+
+    multipleCatchExample();
+
+    try {
+      propagateExample();
+    } catch (Exception ex) {
+      System.out.println("Catch propagateExample");
+    }
+
+    try {
+      printFile();
+    } catch (IOException e) {
+      System.out.println("Catch printFile");
+    }
+
+    try {
+      printFileJava7();
+    } catch (IOException e) {
+      System.out.println("Catch printFileJava7");
+    }
+  }
+
+  private static void basicExample() {
+    try {
+      System.out.println(5 / 0);
+    } catch (Exception ex) {
+      System.out.println("Can't divide by 0");
+    }
+  }
+
+  private static void multipleCatchExample() {
+    try {
+      System.out.println(5 / 0);
+      InputStream input = null;
+      input = new FileInputStream("file.txt");  // code can throw an Exception.
+    } catch (ArithmeticException ex) {
+      System.out.println("Can't divide by 0");
+    } catch (IOException ex) {
+      System.out.println("Problem opening file");
+    } catch (Exception ex) {
+      System.out.println("Catch all other exceptions");
+    }
+  }
+
+  private static void propagateExample() {
+    System.out.println(5 / 0);
+  }
+
+  // Resource Management With Try-Catch-Finally, Old School Style
+  // http://tutorials.jenkov.com/java-exception-handling/try-with-resources.html
+  private static void printFile() throws IOException {
+    InputStream input = null;
+
+    try {
+      input = new FileInputStream("file.txt");  // code can throw an Exception.
+
+      int data = input.read();
+      while (data != -1) {
+        System.out.print((char) data);
+        data = input.read();  // code can throw an Exception.
+      }
+    } finally { //  always executed
+      if (input != null) {
+        input.close();  // code can throw an Exception.
+      }
+    }
+  }
+
+  private static void printFileJava7() throws IOException {
+
+    // try-with-resources construct
+    try (FileInputStream input = new FileInputStream("file.txt")) {
+
+      int data = input.read();
+      while (data != -1) {
+        System.out.print((char) data);
+        data = input.read();
+      }
+    }
+  }
+
+}
